8217b86cff90cfbd68e97dd67f76a6a0c6117287
Jeremy Wang <tomatocat@google.com>
2020-06-19 20:27:41 +0000

    initial commit

---
+++ src/test/java/com/google/sps/SpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.sps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
