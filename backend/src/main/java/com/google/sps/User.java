78d2e393bca5a67163eb7d06af5c1c7e44d68faa
Jeremy Wang <tomatocat@google.com>
2020-06-20 23:41:32 +0000

    to fix

---
+++ backend/src/main/java/com/google/sps/User.java
@@ -0,0 +1,31 @@
+package com.google.sps;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String username;
+
+    protected User() {}
+
+    public User(String email, String username) {
+        this.email = email;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Email: %s, Username: %s",
+                email, username);
+    }
+}
---
+++ backend/src/main/java/com/google/sps/UserController.java
@@ -0,0 +1,15 @@
+package com.google.sps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/users")
+    String users() {
+        return "Hello get";
+    }
+}
