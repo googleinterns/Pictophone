d5cf24564a2831ceba809453ed6ea42c2788ef32
Phoebe Liang <phoebeliang@google.com>
2020-06-25 13:21:46 -0400

    Reorganize code so that routing code is more encapsulated.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
+import Banner from './Banner';
+
 class Home extends Component {
   render() {
     return (
       <div className="Home">
+        <Banner />
+        <hr/>
         Hello world! This is the home page.
         <Link to="/game"><button>Go to game</button></Link>
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,31 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Home from './Home';
-import Banner from './Banner';
 import * as serviceWorker from './serviceWorker';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
 } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Banner />
-    <hr />
     <Router>
-      <Switch>
-
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route path="/game">
-          <App />
-        </Route>
-
-      </Switch>
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
