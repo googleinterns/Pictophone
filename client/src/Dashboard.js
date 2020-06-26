60b2a40c0f946ff06d80b587590a01d23e32049a
Phoebe Liang <phoebeliang@google.com>
2020-06-25 20:25:33 -0400

    Add navbar to dashboard.

---
+++ client/src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './Banner';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
+
