c944a665387c344672c01645a43cd5fd3c000c78
Phoebe Liang <phoebeliang@google.com>
2020-06-26 17:13:38 -0400

    Replace button with Bootstrap button for better styling.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <div className="title">PICTOPHONE</div>
-        <div className="login">
-          <Link to="/dashboard"><button className="login-button">Login</button></Link>
+      <div className="home-wrapper">
+        <div className="home-title">PICTOPHONE</div>
+        <div className="login-button">
+          <Link to="/dashboard"><Button variant="info" size="lg">Login</Button></Link>
         </div>
       </div>
     );
