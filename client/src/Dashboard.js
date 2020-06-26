e2328601d518be8e4a7c04a70ce53d0d16b46b79
Phoebe Liang <phoebeliang@google.com>
2020-06-26 15:03:51 -0400

    Add buttons.

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Banner from './Banner';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './Dashboard.css';
 
-class Dashboard extends React.Component {
+class HostButton extends Component {
   render() {
     return (
-      <div className="wrapper">
+      <Button variant="secondary">host</Button>
+    );
+  }
+}
+
+class JoinButton extends Component {
+  render() {
+    return (
+      <Button variant="secondary">join</Button>
+    );
+  }
+}
+
+class RandomButton extends Component {
+  render() {
+    return (
+      <Button variant="secondary">random</Button>
+    );
+  }
+}
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="banner-wrapper">
         <Banner />
         <div className="greeting">
           <p>Hi, Bob! Here are your games:</p>
+        </div>
+        <div className="dashboard-buttons">
+          <HostButton />{' '}
+          <JoinButton />{' '}
+          <RandomButton />
         </div>
       </div>
     );
