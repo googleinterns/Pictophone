3f19bb76d893006c8d023f4c368985f9994affea
Phoebe Liang <phoebeliang@google.com>
2020-06-25 20:59:22 -0400

    Add greeting message.

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from './Banner';
+import './Dashboard.css';
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <div>
+      <div className="wrapper">
         <Banner />
+        <div className="greeting">
+          <p>Hi, Bob! Here are your games:</p>
+        </div>
       </div>
     );
   }
