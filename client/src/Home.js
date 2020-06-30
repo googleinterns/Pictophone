a23bfc19a89a0ed2de3dd404606f0748a830904d
Phoebe Liang <phoebeliang@google.com>
2020-06-30 13:40:31 -0400

    Extract log-in button and implement custom styling.

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 //import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { GoogleLogin } from 'react-google-login';
-import { GoogleLogout } from 'react-google-login';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
 
@@ -33,13 +32,13 @@ class HomeButton extends Component {
         <GoogleLogin
           clientId="148022077758-4iklfh5g74id8aisgg8qqh8ckboa1qkc.apps.googleusercontent.com"
           render={renderProps => (
-            <Button variant="info" size="lg" onClick={renderProps.onclick} disabled={renderProps.disabled}>Login</Button>
+            <Button variant="info" size="lg" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</Button>
           )}
           buttonText="Login"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'}
-        />;
+        />
     }
 
     return (
@@ -65,16 +64,7 @@ class Home extends Component {
       <div className="home-wrapper">
         <div className="home-title">PICTOPHONE</div>
         <div className="login-button">
-          <GoogleLogin
-            clientId="148022077758-4iklfh5g74id8aisgg8qqh8ckboa1qkc.apps.googleusercontent.com"
-            render={renderProps => (
-              <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>
-            )}
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-          />
+          <HomeButton signinStatus={this.props.signinStatus} onStatusChange={this.changeLogInStatus} />
         </div>
       </div>
     );
