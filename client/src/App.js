bd54f6dca8679974954366893182ff07f4258a06
Phoebe Liang <phoebeliang@google.com>
2020-06-30 13:33:44 -0400

    Rearrange routing for easier prop passing and implement basic Google Sign-In button.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Main from './Main';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Canvas from './Canvas';
 
-function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignedIn: false
+    };
+    this.changeLogInStatus = this.changeLogInStatus.bind(this);
+  }
+
+  changeLogInStatus(status) {
+    this.setState({
+      isSignedIn: status
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Home
+              signinStatus={this.state.isSignedIn}
+              onLogIn={this.changeLogInStatus}
+            />
+          </Route>
+          <Route exact path='/dashboard'>
+            <Dashboard
+              signinStatus={this.state.isSignedIn}
+              onLogOut={this.changeLogInStatus}
+            />
+          </Route>
+          <Route path='/game/:id'> <Canvas /> </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,80 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+//import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { GoogleLogin } from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
 
-class Home extends Component {
-  render() {
+const responseGoogle= (response) => {
+  console.log(response);
+}
 
+class HomeButton extends Component {
+  constructor(props) {
+    super(props);
+    this.changeLogInStatus = this.changeLogInStatus.bind(this);
+  }
+
+  changeLogInStatus() {
+    this.props.onStatusChange();
+  }
+
+  render() {
+    const isSignedIn = this.props.signinStatus;
+    let button;
+
+    if (isSignedIn) {
+      button = <Link to="/dashboard"><Button variant="info" size="lg" id="loginButton">Enter</Button></Link>;
+    }
+    else {
+      button =
+        <GoogleLogin
+          clientId="148022077758-4iklfh5g74id8aisgg8qqh8ckboa1qkc.apps.googleusercontent.com"
+          render={renderProps => (
+            <Button variant="info" size="lg" onClick={renderProps.onclick} disabled={renderProps.disabled}>Login</Button>
+          )}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />;
+    }
+
+    return (
+      <div>
+        {button}
+      </div>
+    );
+  }
+}
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.changeLogInStatus = this.changeLogInStatus.bind(this);
+  }
+
+  changeLogInStatus() {
+    this.props.onLogIn(true);
+  }
+
+  render() {
     return (
       <div className="home-wrapper">
         <div className="home-title">PICTOPHONE</div>
         <div className="login-button">
-          <Link to="/dashboard"><Button variant="info" size="lg">Login</Button></Link>
+          <GoogleLogin
+            clientId="148022077758-4iklfh5g74id8aisgg8qqh8ckboa1qkc.apps.googleusercontent.com"
+            render={renderProps => (
+              <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>
+            )}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
         </div>
       </div>
     );
