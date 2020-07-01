51d3f5d0168f2591c062d6d66c86ff182a11b5d2
anthonyodum <anthony.odum14@gmail.com>
2020-07-01 15:43:28 -0400

    Set up OAuth

--- api/src/main/java/com/google/sps/Application.java
+++ api/src/main/java/com/google/sps/Application.java
@@ -5,13 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 @SpringBootApplication
 @RestController
+@WebServlet("/notify")
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    SpringApplication.run(Application.class, args);
+
+    try {
+      Notifications.sendNotification();
+    } catch(Exception e) {
+      System.out.println("Working Directory = " + System.getProperty("user.dir"));
+      System.out.println("Exception Caught Here: " + e);
+    }
+  }
+
+  public void sendNotifications(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String link = request.getParameter("gameLink");
+    
+  }
 
 	@GetMapping("/")
 	public String hello() {
---
+++ api/src/main/java/com/google/sps/Notifications.java
@@ -0,0 +1,141 @@
+package com.google.sps;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.util.ResourceUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.*;
+
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.auth.http.HttpCredentialsAdapter;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class Notifications {
+  private static final String APPLICATION_NAME = "Notifications";
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final String TOKENS_DIRECTORY_PATH = "tokens123";
+
+  /**
+   * Global instance of the scopes required by this quickstart. If modifying these
+   * scopes, delete your previously saved tokens/ folder.
+   */
+  private static final List<String> SCOPES = Collections.singletonList(GmailScopes.MAIL_GOOGLE_COM);
+  private static final String CREDENTIALS_FILE_PATH = "./credentials.json";
+
+  /**
+   * Creates an authorized Credential object.
+   *
+   * @param HTTP_TRANSPORT The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException If the credentials.json file cannot be found.
+   */
+  private static HttpRequestInitializer getCredentials(NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    // Load client secrets.
+    Path currentRelativePath = Paths.get("");
+    String s = currentRelativePath.toAbsolutePath().toString();
+    System.out.println("Current relative path is: " + s);
+
+    File file = ResourceUtils.getFile(CREDENTIALS_FILE_PATH);
+    if (file == null) {
+      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+    }
+    InputStream in = new FileInputStream(file);
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+        clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline").build();
+    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+
+    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+  }
+
+  private static MimeMessage createEmail(String to, String from, String subject, String bodyText)
+      throws MessagingException {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+
+    MimeMessage email = new MimeMessage(session);
+
+    email.setFrom(new InternetAddress(from));
+    email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+    email.setSubject(subject);
+    email.setText(bodyText);
+    return email;
+  }
+
+  private static Message createMessageWithEmail(MimeMessage emailContent) throws MessagingException, IOException {
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    emailContent.writeTo(buffer);
+    byte[] bytes = buffer.toByteArray();
+    String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+    Message message = new Message();
+    message.setRaw(encodedEmail);
+    return message;
+  }
+
+  private static Message sendMessage(Gmail service, String userId, MimeMessage emailContent)
+      throws MessagingException, IOException {
+    Message message = createMessageWithEmail(emailContent);
+    message = service.users().messages().send(userId, message).execute();
+
+    System.out.println("Message id: " + message.getId());
+    System.out.println(message.toPrettyString());
+        return message;
+    }
+
+    public static void sendNotification() throws IOException, GeneralSecurityException {
+      // Build a new authorized API client service.
+      final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+      Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+              .setApplicationName(APPLICATION_NAME)
+              .build();
+
+      String body = "https://google.com";
+      String subject = "test";
+      String to = "pictophone.noreply@gmail.com";
+      String from = "pictophone.noreply@gmail.com";
+
+      try {
+        MimeMessage encoded = createEmail(to, from, subject, body);
+        Message testMessage = sendMessage(service, from, encoded);
+
+        System.out.println(testMessage);
+      } catch(Exception e) {
+        System.out.println("Main Method Exception: " + e);
+      }
+    }
+}
