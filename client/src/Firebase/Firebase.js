1eaed01af7e124bb34968db921dd1e6d707bcada
Phoebe Liang <phoebeliang@google.com>
2020-07-07 16:40:49 -0400

    Implement auth persistence.

--- client/src/Firebase/Firebase.js
+++ client/src/Firebase/Firebase.js
@@ -42,6 +42,29 @@ class Firebase {
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
 
+  // *** Merge Auth and DB User API *** //
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.user(authUser.uid)
+          .get()
+          .then(snapshot => {
+            const dbUser = snapshot.data();
+
+            // merge auth and db user
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              ...dbUser,
+            };
+
+            next(authUser);
+          });
+      } else {
+        fallback();
+      }
+    });
+
   // *** User API ***
 
   user = uid => this.db.doc(`users/${uid}`);
