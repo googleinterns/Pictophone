4c46d68fa188fad93cd29d2c159531ad9edded51
Phoebe Liang <phoebeliang@google.com>
2020-07-07 00:03:37 -0400

    Add higher order component for authorization.

---
+++ client/src/Session/withAuthorization.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../constants/routes';
+
+const withAuthorization = condition => Component => {
+  class WithAuthorization extends React.Component {
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          if (!condition(authUser)) {
+            this.props.history.push(ROUTES.LANDING);
+          }
+        },
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
+
+    render() {
+      return <Component { ...this.props } />;
+    }
+  }
+
+  return compose(
+    withRouter,
+    withFirebase,
+  )(withAuthorization);
+};
+
+export default withAuthorization;
