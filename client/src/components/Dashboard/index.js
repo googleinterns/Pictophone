cc22ee9ae393d09ca9d2687eae34e2e9f1e45a8a
Phoebe Liang <phoebeliang@google.com>
2020-07-08 18:33:08 -0400

    Fetch user data from backend.

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -5,6 +5,7 @@ import { compose } from 'recompose';
 import Banner from '../Banner';
 import GameSelector from '../GameSelector';
 import { withAuthorization, withEmailVerification } from '../Session';
+import { withFirebase } from '../Firebase';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './Dashboard.css';
@@ -34,24 +35,61 @@ class RandomButton extends Component {
 }
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      user: null,
+      ...props.location.state,
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.user) {
+      return;
+    }
+
+    this.setState({ loading: true });
+
+    this.unsubscribe = this.props.firebase
+      .user(this.props.match.params.id)
+      .onSnapshot(snapshot => {
+        this.setState({
+          user: snapshot.data(),
+          loading: false,
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe && this.unsubscribe();
+  }
+
   render() {
     // Hard coded for now, these games would be taken from the database
     const games = ['oo0j05CCUJNqQxSFSNEI'];
+    const { user, loading } = this.state;
 
     return (
       <div className="banner-wrapper">
         <Banner />
-        <div className="greeting">
-          <p>Hi, Sherb! Here are your games:</p>
-        </div>
-        <div className="dashboard-buttons">
-          <HostButton />{' '}
-          <JoinButton />{' '}
-          <RandomButton />
-        </div>
-        <div className="games-list">
-          <GameSelector id={games[0]}/>
-        </div>
+        {loading && <div>Loading...</div>}
+        {user && (
+          <div>
+            <div className="greeting">
+              <p>Hi, {user.username}! Here are your games:</p>
+            </div>
+            <div className="dashboard-buttons">
+              <HostButton />{' '}
+              <JoinButton />{' '}
+              <RandomButton />
+            </div>
+            <div className="games-list">
+              <GameSelector id={games[0]}/>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
@@ -62,4 +100,5 @@ const condition = authUser => !!authUser;
 export default compose(
   withEmailVerification,
   withAuthorization(condition),
+  withFirebase,
 )(Dashboard);
