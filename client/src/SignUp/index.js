e4804020aec9c9d85a8e6c36b95e39d01e2de325
Phoebe Liang <phoebeliang@google.com>
2020-07-08 13:04:48 -0400

    Change placeholder in form.

--- client/src/SignUp/index.js
+++ client/src/SignUp/index.js
@@ -83,7 +83,7 @@ class SignUpFormBase extends Component {
           value={username}
           onChange={this.onChange}
           type="text"
-          placeholder="Full Name"
+          placeholder="Username"
         />
         <input
           name="email"
